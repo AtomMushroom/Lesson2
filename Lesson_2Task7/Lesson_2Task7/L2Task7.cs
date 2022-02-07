25fa7b649ffd42d5c2a12036603d7868e20e68d4
AtomMushroom <35073893+AtomMushroom@users.noreply.github.com>
2022-02-07 18:04:23 +0300

    Add files via upload

---
+++ Lesson_2Task7/Lesson_2Task7/L2Task7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson_2Task7
+{
+	class L2Task7
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите число: ");
+			int x = int.Parse(Console.ReadLine());
+			if (x<=0)
+			{
+				Console.WriteLine(6 - x);
+			}
+			else
+			{
+				Console.WriteLine(2 * Math.Sin(x));
+			}
+		}
+	}
+}
---
+++ Lesson_2Task8/Lesson_2Task8/L2Task8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lesson_2Task8
+{
+	class L2Task8
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите число от 1 до 7: ");
+			string choice = Console.ReadLine();
+			string day;
+
+			switch (choice)
+			{
+				case "1":
+					Console.WriteLine("Черный понедельник...");
+					break;
+				case "2":
+					Console.WriteLine("Вторник");
+					break;
+				case "3":
+					Console.WriteLine("Среда");
+					break;
+				case "4":
+					Console.WriteLine("Четверг");
+					break;
+				case "5":
+					Console.WriteLine("УРААА ПЯТНИЦА");
+					break;
+				case "6":
+					Console.WriteLine("УРААА СУББОТА");
+					break;
+				case "7":
+					Console.WriteLine("УРААА ВОСКРЕСЕНЬЕ");
+					break;
+				default:
+					Console.Write("Дня недели не существует!");
+					break;
+			}
+
+		}
+	}
+}
